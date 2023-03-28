72b83d16d861167c92674603aff0380f975c901b
codingwithsaani <codingwithsaani@gmail.com>
2023-03-28 09:36:28 +0500

    Lecture 5

---
+++ app/src/main/java/com/example/spring23e1app/Lecture5/Lec5Activity.kt
@@ -0,0 +1,46 @@
+package com.example.spring23e1app.Lecture5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.spring23e1app.R
+
+class Lec5Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lec5)
+
+        var firstValueET:EditText = findViewById(R.id.first_value_et)
+        var secondVlaueET:EditText = findViewById(R.id.second_value_et)
+
+        var magicBtn: Button = findViewById(R.id.magic_btn)
+        var resultTV: TextView = findViewById(R.id.result_tv)
+
+        magicBtn.setOnClickListener {
+
+            if(firstValueET.text.toString().isNotEmpty() &&
+                secondVlaueET.text.toString().isNotEmpty())
+            {
+                var firstValue = firstValueET.text.toString().toInt()
+                var secondValue = secondVlaueET.text.toString().toInt()
+
+                var result = firstValue+secondValue
+                resultTV.text = "${getString(R.string.result_tv)} $result"
+            }
+            else if(firstValueET.text.toString().isEmpty())
+            {
+
+            }
+            else if(secondVlaueET.text.toString().isEmpty())
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
